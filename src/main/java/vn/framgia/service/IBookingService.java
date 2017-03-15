ce24adf016be32245fe0e38ea3a8ac7bfc1a6c61
NgoVanDuy <ngo.van.duy@framgia.com>
2017-03-15 17:24:05 +0700

    searbooking by some addition cilent

--- src/main/java/vn/framgia/service/IBookingService.java
+++ src/main/java/vn/framgia/service/IBookingService.java
@@ -7,5 +7,5 @@ import vn.framgia.bean.ShowBookingBean;
 
 public interface IBookingService extends Serializable{
 	public List<List<ShowBookingBean>> findAllBookingOrderByDesc();
-	
+	public List<List<ShowBookingBean>> searchBookingByNameClient(String nameClient);
 }
